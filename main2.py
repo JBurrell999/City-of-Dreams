1858aa0ca11bd4683224a9adb8f69888b1d43bed
JBurrell999 <134342563+JBurrell999@users.noreply.github.com>
2024-04-05 10:54:36 -0700

    Add files via upload

---
+++ main2.py
@@ -0,0 +1,39 @@
+from dotenv import load_dotenv
+import os
+import streamlit as st
+import pandasai.pandas as pd
+from pandasai import SmartDataframe
+from pandasai.llm.openai import OpenAI
+import matplotlib
+
+matplotlib.use('Agg')
+
+load_dotenv()
+
+API_KEY = os.environ.get('OPENAI_API_KEY', 'your_openai_api_key_here')
+
+os.environ['PANDASAI_API_KEY'] = '$2a$10$61Xl5tmhXWBg77y9ByiUc.uGps4.HsBgNh8yZpkdcSgBOnKgtNfrK'
+
+llm = OpenAI(api_token=API_KEY)
+
+st.title("Data Analysis for City of Dreams")
+uploaded_file = st.file_uploader("Upload a csv file for analysis", type=['csv'])
+
+if uploaded_file is not None:
+    df = pd.read_csv(uploaded_file)
+    st.write(df.head(10))
+
+    try:
+        pandas_ai = SmartDataframe(df)
+        prompt = st.text_area("Enter Prompt:")
+        
+        if st.button("Generate"):
+            if prompt:
+                with st.spinner("City of Dream's Analysis Tool is generating an answer, please wait"):
+                    # Adjusted call to chat method without using keyword argument for prompt
+                    result = pandas_ai.chat(prompt)
+                    st.write(result)
+            else:
+                st.warning("Please enter a prompt.")
+    except Exception as e:
+        st.error(f"An error occurred: {e}")
